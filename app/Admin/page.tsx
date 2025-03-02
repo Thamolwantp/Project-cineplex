ac43998bdb060dd9805e2bf43fc9d4f9109bf9f5
Chanon0000 <66022679@up.ac.th>
2025-03-02 22:52:19 +0700

    ส่งเมลสำเร็จ(ซื้อตั๋วBK)

--- app/Admin/page.tsx
+++ app/Admin/page.tsx
@@ -1,8 +1,8 @@
 "use client";
-
+import Head from 'next/head';
 import React, { useEffect, useState } from "react";
-import MovieCard from "../admin/MovieCard";
-import "./admind.css";
+import MovieCard from "./MovieCard";
+import "./admin.css";
 import Navbar from "@/components/ForAdmin/NavbarAM";
 import Footter from "@/components/Footter";
 
